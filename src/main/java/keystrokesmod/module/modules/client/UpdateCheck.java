7d5a10dc263ff877fd6029264dfc0c5a9f058a1a
JMRaich <agentmimosadev@gmail.com>
2022-02-06 10:41:23 +0100

    Reorganized code

--- src/main/java/keystrokesmod/module/modules/client/UpdateCheck.java
+++ src/main/java/keystrokesmod/module/modules/client/UpdateCheck.java
@@ -22,38 +22,7 @@ public class UpdateCheck extends Module {
 
     private Future<?> f;
     private final ExecutorService executor;
-    private final Runnable task = () -> {
-        if (Version.outdated()) {
-            Ravenbplus.outdated = true;
-            Utils.Player.sendMessageToSelf("The current version or Raven B+ is outdated. Visit https://github.com/Kopamed/Raven-bPLUS to download the latest version.");
-            Utils.Player.sendMessageToSelf("https://github.com/Kopamed/Raven-bPLUS");
-        }
-
-        if (Version.isBeta()) {
-            Ravenbplus.beta = true;
-            Utils.Player.sendMessageToSelf("Man is on beta and asking for stable. You mad bruv?");
-            Utils.Player.sendMessageToSelf("https://github.com/Kopamed/Raven-bPLUS");
-        } else {
-            Utils.Player.sendMessageToSelf("You are on the latest public version!");
-        }
-
-        if (copyToClipboard.isToggled()) {
-            if (Utils.Client.copyToClipboard(Ravenbplus.sourceLocation))
-                Utils.Player.sendMessageToSelf("Successfully copied download link to clipboard!");
-        }
-
-        if (openLink.isToggled()) {
-            try {
-                URL url = new URL(Ravenbplus.sourceLocation);
-                Utils.Client.openWebpage(url);
-            } catch (MalformedURLException bruh) {
-                bruh.printStackTrace();
-                Utils.Player.sendMessageToSelf("&cFailed to open page! Please report this bug in Raven b+'s discord");
-            }
-        }
-
-        this.disable();
-    };
+    private final Runnable task;
 
     public UpdateCheck() {
         super("Update", category.client, 0);
@@ -63,6 +32,37 @@ public class UpdateCheck extends Module {
         this.registerSetting(openLink = new ModuleSettingTick("Open dl in browser", true));
 
         executor = Executors.newFixedThreadPool(1);
+        task = () -> {
+            if (Version.outdated()) {
+                Ravenbplus.outdated = true;
+                Utils.Player.sendMessageToSelf("The current version or Raven B+ is outdated. Visit https://github.com/Kopamed/Raven-bPLUS to download the latest version.");
+                Utils.Player.sendMessageToSelf("https://github.com/Kopamed/Raven-bPLUS");
+            }
+
+            if (Version.isBeta()) {
+                Ravenbplus.beta = true;
+                Utils.Player.sendMessageToSelf("Man is on beta and asking for stable. You mad bruv?");
+                Utils.Player.sendMessageToSelf("https://github.com/Kopamed/Raven-bPLUS");
+            } else {
+                Utils.Player.sendMessageToSelf("You are on the latest public version!");
+            }
+
+            if (copyToClipboard.isToggled())
+                if (Utils.Client.copyToClipboard(Ravenbplus.sourceLocation))
+                    Utils.Player.sendMessageToSelf("Successfully copied download link to clipboard!");
+
+            if (openLink.isToggled()) {
+                try {
+                    URL url = new URL(Ravenbplus.sourceLocation);
+                    Utils.Client.openWebpage(url);
+                } catch (MalformedURLException bruh) {
+                    bruh.printStackTrace();
+                    Utils.Player.sendMessageToSelf("&cFailed to open page! Please report this bug in Raven b+'s discord");
+                }
+            }
+
+            this.disable();
+        };
     }
 
     @SubscribeEvent
