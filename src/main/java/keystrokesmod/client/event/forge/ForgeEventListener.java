e036aec1fb4181744d140e01334cb02ab989a610
K-v <58765519+K-ov@users.noreply.github.com>
2022-10-06 22:52:25 +1100

    hope this works

--- src/main/java/keystrokesmod/client/event/forge/ForgeEventListener.java
+++ src/main/java/keystrokesmod/client/event/forge/ForgeEventListener.java
@@ -7,10 +7,14 @@ import keystrokesmod.client.main.Raven;
 import keystrokesmod.client.module.Module;
 import keystrokesmod.client.utils.Utils;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.*;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.DrawBlockHighlightEvent;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderLivingEvent.Specials;
 import net.minecraftforge.client.event.RenderPlayerEvent.Post;
 import net.minecraftforge.client.event.RenderPlayerEvent.Pre;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
@@ -28,31 +32,29 @@ public class ForgeEventListener {
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent e) {
-        if (e.phase == TickEvent.Phase.END) {
-            if (Utils.Player.isPlayerInGame()) {
-                for (Module module : Raven.moduleManager.getModules()) {
-                    if (Minecraft.getMinecraft().currentScreen instanceof ClickGui) {
+        if (e.phase == TickEvent.Phase.END)
+            if (Utils.Player.isPlayerInGame())
+                for (Module module : Raven.moduleManager.getModules())
+                    if (Minecraft.getMinecraft().currentScreen instanceof ClickGui)
                         module.guiUpdate();
-                    }
-                }
-            }
-        }
     }
 
     @SubscribeEvent
     public void onRenderTick(TickEvent.RenderTickEvent e) {
         if (e.phase == TickEvent.Phase.END) {
-            if (Utils.Player.isPlayerInGame()) {
-                for (Module module : Raven.moduleManager.getModules()) {
-                    if (Minecraft.getMinecraft().currentScreen == null) {
+            if (Utils.Player.isPlayerInGame())
+                for (Module module : Raven.moduleManager.getModules())
+                    if (Minecraft.getMinecraft().currentScreen == null)
                         module.keybind();
-                    }
-                }
-            }
 
             Raven.eventBus.post(new Render2DEvent());
         }
 
+        Raven.eventBus.post(new ForgeEvent(e));
+    }
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent e) {
         Raven.eventBus.post(new ForgeEvent(e));
     }
 
