bf9078a983c7be117d2fbf0773bfa3936e350fce
K-v <58765519+K-ov@users.noreply.github.com>
2022-10-17 18:01:17 +1100

    a

---
+++ src/main/java/keystrokesmod/client/utils/RotationUtil.java
@@ -0,0 +1,15 @@
+package keystrokesmod.client.utils;
+
+import net.minecraft.client.Minecraft;
+
+public class RotationUtil {
+
+    public static final Minecraft mc = Minecraft.getMinecraft();
+
+    public static float pitchToBlockFace(float x, float y, float z) {
+
+
+
+        return 70;
+    }
+}
